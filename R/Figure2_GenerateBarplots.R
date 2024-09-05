c2d16a6dc8e9a66b538074b186be0b2e0f7e576b
do Valle Pereira <dovallej@oregonstate.edu>
2024-09-05 10:46:14 -0700

    Upload running barplots script

--- R/Figure2_GenerateBarplots.R
+++ R/Figure2_GenerateBarplots.R
@@ -1,55 +1,64 @@
-#########################################
-dataTable_driver %>%  
-  group_by(common_name, human_induced, tactic_driver) %>%  
-  summarize(Count = n()) %>%
+# Plot age of class of foraging tactics with photo ID infomation 
+age_plot <- dataTable %>% 
+  dplyr::filter(age_class != "unknown",
+                animal_identity_data == "yes") %>% 
+  dplyr::group_by(sex, age_class) %>%  
+  dplyr::summarize(Count = n()) %>% 
+  ggplot() +
+  geom_col(aes(y = Count, x = sex,  fill = sex)) +
+  xlab("") +
+  ylab("") +
+  facet_wrap(~age_class) +
+  scale_fill_manual(values = c("#000000","#696969","#D3D3D3", "#C0C0C0")) +
+  theme_classic() +
+  theme(legend.position = "none",
+        text = element_text(size=15))
+age_plot
+# ggsave(file="age_plot.svg", plot=age_plot, width=10, height=8)
+
+# Plot transmission mode of the putative cultural foraging tactics
+transmission_plot <- dataTable %>% 
+  dplyr::filter(transmission_direction != "unknown",
+                putative_specialised_foraging_tactic == "yes") %>% 
+  dplyr::mutate(transmission_short = ifelse(transmission_direction != "vertical" &
+                                              transmission_direction != "horizontal",
+                                            "multiple", 
+                                            transmission_direction)) %>% 
+  dplyr::group_by(transmission_short) %>%  
+  dplyr::summarize(Count = n()) %>% 
+  ggplot() +
+  geom_col(aes(y = Count, x = transmission_short,  fill = transmission_short)) +
+  xlab("Transmission direction") +
+  ylab("") +
+  scale_fill_manual(values = c("#000000","#D3D3D3", "#808080"),
+                    name = "Transmission direction") +
+  theme_classic() +
+  theme(legend.position = "none",
+        text = element_text(size=15))
+transmission_plot
+# ggsave(file="transmission_plot.svg", plot=transmission_plot, width=10, height=8)
+
+# Plot tactic reliance on the nature of their prey or physical structures 
+tactic_driver_plot <- dataTable %>%  
+  dplyr::group_by(common_name, tactic_driver) %>%  
+  dplyr::summarize(Count = n()) %>%
   ggplot() +
   geom_bar(aes(y = forcats::fct_reorder(common_name, Count, .desc = TRUE),
                x = Count,
-               fill = human_induced), stat = "identity") +
+               fill = tactic_driver), stat = "identity") +
   scale_fill_manual(values = c("#696969","#D3D3D3"), 
-                    name = "Tactic driver",
-                    guide = guide_legend(label.theme = element_text(face = "italic"))) +
-  ylab("Species") +
+                    name = "Tactic driver") +
+  scale_x_continuous(expand = c(0,0)) +
+  ylab("") +
   xlab("Number of cases") +
-  theme_test() +
-  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) 
+  theme_bw() +
+  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0))) +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        text = element_text(size=15)) 
+tactic_driver_plot
+# ggsave(file = "tactic_driver_plot.svg", plot = tactic_driver_plot, width=10, height=8)
 
-
-# Plot transmission direc
-
-dataTable_transmission <- dataTable %>% 
-  dplyr::filter(!is.na(transmission_direction),
-                transmission_direction != "unknown",
-putative_specialised_foraging_tactic == "yes") %>% 
-  dplyr::mutate(transmission_short = ifelse(transmission_direction != "vertical" &
-                                              transmission_direction != "horizontal", "multiple", transmission_direction))
-
-
-dataTable_transmission %>%  
-  group_by(common_name, transmission_short) %>%  
-  summarize(Count = n()) %>% 
-  ggplot() +
-  geom_bar(aes(y = Count, x = transmission_short, fill = transmission_short), stat = "identity", position = "dodge") +
-  scale_fill_manual(values = colorblind_safe_cat, 
-                    name = "Transmission direction") +
-  theme_test() +
-  ylab("Number of cases") +
-  xlab("Species") +
-  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
-        axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)))
-
-descriptive_plots <- ggpubr::ggarrange(tactic_broad,tactic_specific, transmission_direction, tactic_driver)
-ggsave("descriptive_plots.PNG", width = 240, height = 200, dpi = 600)
-
-dataTable_transmission %>%  
-  group_by(common_name, transmission_short) %>%  
-  summarize(Count = n()) %>% 
-  ggplot() +
-  geom_bar(aes(y = common_name, x = Count, fill = transmission_short), stat = "identity", position = "dodge") +
-  scale_fill_manual(values = colorblind_safe_cat, 
-                    name = "Transmission direction") +
-  theme_test() +
-  ylab("Number of cases") +
-  xlab("Species") +
-  theme(axis.title.y = element_text(margin = margin(t = 0, r = 20, b = 0, l = 0)),
-        axis.title.x = element_text(margin = margin(t = 20, r = 0, b = 0, l = 0)))
--- R/Load_data.R
+++ R/Load_data.R
@@ -1,7 +1,8 @@
 # Load and clean table ----
 
-#
 dataTable <- read.csv("./data/240904_PhilTrans_Lab_Dataset.csv") %>% 
   janitor::clean_names() %>% 
   dplyr::mutate_if(is.character, str_trim)
 head(dataTable)
+
+
