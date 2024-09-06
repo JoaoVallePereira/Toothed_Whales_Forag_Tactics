b5c1bd2c6b9230bfc2d742f9fdd299ce1f108156
Michaela Kratofil <61757397+makratofil@users.noreply.github.com>
2024-09-05 17:30:03 -0700

    fixed column formatting

--- R/Load_data.R
+++ R/Load_data.R
@@ -9,16 +9,15 @@ head(dataTable)
 # Load and clean map table names ----
 
 dataTable_map <- read.csv("./data/dataTable_Forag_Tactics_map.csv") %>% 
-  janitor::clean_names() %>% 
+janitor::clean_names() %>%
   dplyr::rename(common_name = spp_common,
                 latin_name = spp_latin,
                 country = map_region,
                 region = map_subregion,
                 putative_specialised_foraging_tactic = multan_id_learn,
-                foraging_category = tactic_category,
-                evidence_for_discreteness_significance = evidence
+                foraging_category = tactic_category
                 )
-head(dataTable_map)
+# head(dataTable_map)
 
 
 
